c8eb20b88d425a5a2d4fc6ca892cdbabe4f015a8
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-10-19 22:55:04 +0530

    commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -67,7 +67,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.jpg"
+              src="/images/hero-image.jpeg"
               alt="hero image"
               className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
